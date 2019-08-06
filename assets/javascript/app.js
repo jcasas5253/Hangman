8271463c6692ec0aceea8cc405b8672f0565f03d
jcasas5253 <43353267+jcasas5253@users.noreply.github.com>
2019-08-05 17:04:05 -0700

    new logic

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,17 +1,29 @@
-function selfDestruct() {
-    $('.dead-left-eye').css("display", "block");
-    $('.dead-left-eye-two').css("display", "block");
-    $('.dead-right-eye').css("display", "block");
-    $('.dead-right-eye-two').css("display", "block");
-    $('.alive-left-eye').css("display", "none");
-    $('.alive-right-eye').css("display", "none");
+var words = ["abruptly", "absurd", "abyss", "affix", "askew", "avenue", "awkward", "axiom", "azure", "bagpipes", "bandwagon", "banjo", "bayou", "beekeeper", "bikini", "blitz", "blizzard", "boggle", "bookworm", "boxcar", "boxful", "buckaroo", "buffalo", "buffoon", "buxom", "buzzard", "buzzing", "buzzwords", "caliph", "cobweb", "cockiness", "croquet", "crypt", "curacao", "cycle", "daiquiri", "dirndl", "disavow", "dizzying", "duplex", "dwarves", "embezzle", "equip", "espionage", "euouae", "exodus", "faking", "fishhook", "fixable", "fjord", "flapjack", "flopping", "fluffiness", "flyby", "foxglove", "frazzled", "frizzled", "fuchsia", "funny", "gabby", "galaxy", "galvanize", "gazebo", "giaour", "gizmo", "glowworm", "glyph", "gnarly", "gnostic", "gossip", "grogginess",
+"haiku", "haphazard", "hyphen", "iatrogenic", "icebox", "injury", "ivory", "ivy", "jackpot", "jaundice", "jawbreaker", "jaywalk", "jazziest", "jazzy", "jelly", "jigsaw", "jinx", "jiujitsu", "jockey", "jogging", "joking", "jovial", "joyful", "juicy", "jukebox", "jumbo", "kayak", "kazoo", "keyhole", "khaki", "kilobyte", "kiosk", "kitsch", "kiwifruit", "klutz", "knapsack", "larynx", "lengths", "lucky", "luxury", "lymph", "marquis", "matrix", "megahertz", "microwave", "mnemonic", "mystify", "naphtha", "nightclub", "nowadays", "numbskull", "nymph", "onyx", "ovary", "oxidize", "oxygen", "pajama", "peekaboo", "phlegm", "pixel", "pizazz", "pneumonia", "polka", "pshaw", "psyche", "puppy", "puzzling", "quartz", "queue", "quips", "quixotic", "quiz", "quizzes", "quorum", "razzmatazz",
+"rhubarb", "rhythm", "rickshaw", "schnapps", "scratch", "shiv", "snazzy", "sphinx", "spritz", "squawk", "staff", "strength", "strengths", "stretch", "stronghold", "stymied", "subway", "swivel", "syndrome", "thriftless", "thumbscrew", "topaz", "transcript", "transgress", "transplant", "triphthong", "twelfth", "twelfths", "unknown", "unworthy", "unzip", "uptown", "vaporize", "vixen", "vodka", "voodoo", "vortex", "voyeurism", "walkway", "waltz", "wave", "wavy", "waxy", "wellspring", "wheezy", "whiskey", "whizzing", "whomever", "wimpy", "witchcraft", "wizard", "woozy", "wristwatch", "wyvern", "xylophone", "yachtsman", "yippee", "yoked", "youthful", "yummy", "zephyr", "zigzag", "zigzagging", "zilch", "zipper", "zodia"]
+
+var random = words[Math.floor(Math.random() * words.length)];
+var lettersInWord = [];
+lettersInWord = random.split("");
+console.log(lettersInWord);
+
+function testing() {
+    var i;
+    for (i = 0; i < lettersInWord.length; i++) {
+        $('.word-container').append("<div class='blank-space'>" + lettersInWord[i] + "</div>");
+    }
 }
 
-function alive() {
-    $('.dead-left-eye').css("display", "none");
-    $('.dead-left-eye-two').css("display", "none");
-    $('.dead-right-eye').css("display", "none");
-    $('.dead-right-eye-two').css("display", "none");
-    $('.alive-left-eye').css("display", "block");
-    $('.alive-right-eye').css("display", "block");
+function A() {
+    var blankSpace = $('.blank-space')
+    var i;
+    for (i = 0; i < blankSpace.length; i++) {
+        if (blankSpace[i].val === "a") {
+            this.css("color", "black");
+        } else {
+            console.log("idk");
+        }
+    }
 }
+
+
